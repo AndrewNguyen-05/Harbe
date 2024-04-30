57a771fd1aefd576efc8594c60fafbe13809f60d
Nguyễn Văn Hoàng Anh <21520144@gm.uit.edu.vn>
2024-05-01 00:03:01 +0700

    Fix bug cart

--- cart-service/src/main/java/com/harbe/cartservice/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.harbe.cartservice.repository;
-
-import com.harbe.cartservice.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CartRepository extends CrudRepository<String, String> {
-}
