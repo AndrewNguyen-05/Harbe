88f15b9b3b4ab93eb614f2ed3131e6cc298a0ec1
Andrew Nguyen <21520144@gm.uit.edu.vn>
2024-06-12 20:40:31 +0700

    Add address query

---
+++ product-service/src/main/resources/db queries/address query.sql
@@ -0,0 +1,4 @@
+INSERT INTO address(id, address, district, name, phone, province, ward, user_id)
+VALUES 
+(1, "Trường Đại Học Công Nghệ Thông Tin, Đường Hàn Thuyên, Khu Phố 6", 'Thành Phố Thủ Đức', 'Admin', '0123123123', "TP. Hồ Chí Minh", "Phường Linh Trung", 1),
+(2, "Ký túc xá khu A, Đường Tạ Quang Bửu", "Thành Phố Thủ Đức, Khu Phố 6", "Nguyễn Văn Hoàng Anh", "0123123123", "TP. Hồ Chí Minh", "Phường Linh Trung", 2)
